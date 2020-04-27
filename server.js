aa0d843dba38730ebffd5952b7585516b86b47a4
Ian Hooper <ianhooper8@gmail.com>
2020-04-27 13:28:16 -0400

    adding post method

--- server.js
+++ server.js
@@ -10,3 +10,34 @@ const PORT = 3000
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+let note = []
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'))
+  })
+
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, 'notes.html'))
+  })
+
+
+app.get('/api/notes', function (req, res) {
+    res.json(note)
+})
+
+
+
+app.post('/api/notes', function (req, res) {
+   
+    const newNote = req.body
+  
+    console.log(newNote)
+  
+    note.push(newNote)
+  
+    res.json(newNote)
+    
+  })
+
+app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`))
+
