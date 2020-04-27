f8b653919ad4cf3ba485967e83f2536347c70cc3
Ian Hooper <ianhooper8@gmail.com>
2020-04-27 01:34:34 -0400

    declaring dependencies andport

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const fs = require('fs')
+
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const PORT = 3000
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
